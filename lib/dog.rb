b47888c51004ba0348fe02f70ad097457915484e
davidmolina2810 <davidmolina2810@gmail.com>
2020-10-08 11:28:05 -0500

    Completed lab.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # Add your code here
+
+class Dog
+
+  attr_reader :name
+  
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    return @@all
+  end
+
+  def self.clear_all
+    return @@all = []
+  end
+
+  def self.print_all 
+    @@all.each do |dog|
+      puts dog.name
+    end
+    nil
+  end
+
+end
